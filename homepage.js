2bcd85f8c5a436ef5c0a16ac6bae35e5ef36b3e5
samue <T0475750@my.ntu.ac.uk>
2025-07-04 21:34:35 +0100

    the programs link in my navbar wasnt working how I wanted. It would just jump to the jointhe game title, and you couldnt really see the program cards without having to scroll more by yoursef. I spent a few days trying to fix this and it finally works now, with help of looking up examples I figured out how to fix it with js. My code now finds the exact position of the cards using .offsetTop, then I subtracted 150 pixels from that spot so it lands exactly above them when it drops down

--- homepage.js
+++ homepage.js
@@ -1,5 +1,18 @@
 
-    AOS.init({
+AOS.init({
     duration: 900,
     once: false,
 });
+
+const programsLink = document.querySelector('a[href="#programs"]');
+programsLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    const targetElement = document.getElementById('program-cards');
+
+        const topPosition = targetElement.offsetTop - 150;
+
+        window.scrollTo({
+            top: topPosition,
+            behavior: 'smooth'
+        });
+    });
